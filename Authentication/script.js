df91c39fe518fa1446664872927d9b4f0642ef59
Mrudul Kolambe <83169198+mrudulkolambe@users.noreply.github.com>
2021-06-22 18:19:36 +0530

    update script.js

--- Authentication/script.js
+++ Authentication/script.js
@@ -1,46 +1,33 @@
-    const hamburger_menu = document.querySelector(".hamburger-menu");
-    const container = document.querySelector(".container");
-
-    hamburger_menu.addEventListener("click", () => {
-      container.classList.toggle("active");
-    });
-
+const hamburger_menu = document.querySelector(".hamburger-menu");
+const container = document.querySelector(".container")
+hamburger_menu.addEventListener("click", () => {
+  container.classList.toggle("active");
+});
 console.log('hello');
-
-
 const auth = firebase.auth();
-
-const SignUp = document.getElementById('SignUp');
-function registerAcc(){
+function registerAcc() {
   let email = document.getElementById('participantEmail').value;
   let pass = document.getElementById('password').value;
   validation(email);
-  
+
   function validation(email) {
-  let domain_name = (email.substring(email.length - 13));
+    let domain_name = (email.substring(email.length - 13));
     if (domain_name == '@sigce.edu.in') {
-      value = 'true';
-    }
-    else{
-      value = 'false';
+      value = true;
+    } else {
+      value = false;
     }
     return value;
   }
-  if (value == 'true) {
-  const promise = auth.createUserWithEmailAndPassword(email, pass);
-  promise.catch(e=> alert(e.message));
-  alert('Signed Up!');
-  }
-  else if('false') {
+  if (value == true) {
+    let promise = auth.createUserWithEmailAndPassword(email, pass);
+    promise.catch(e=> alert(e.message));
+    alert('Signed Up!');
+    document.getElementById('reg_form').reset();
+    setTimeout(function() {
+      window.open('https://sigceevent.ml', '_self');
+    }, 1000);
+  } else {
     alert('Enter The College Email Id!');
   }
 };
-const Login = document.getElementById('Login');
-function signIn(){
-  
-  let email = document.getElementById('participantEmail').value;
-  let pass = document.getElementById('password').value;
-  const promise = auth.signInWithEmailAndPassword(email, pass);
-  promise.catch(e=> alert(e.message));
-  alert('Signed In!' + email);
-}
