dcf5002b506a66250e6e2a5ec024a42b62e9023b
Mrudul Kolambe <83169198+mrudulkolambe@users.noreply.github.com>
2021-06-22 11:21:32 +0530

    update script.js

--- Backend/script.js
+++ Backend/script.js
@@ -1,8 +1,24 @@
 Name = document.getElementById('participantName').value;
-Email = document.getElementById('participantEmail').value;
+email = document.getElementById('participantEmail').value;
 PhoneNo = document.getElementById('participantPNo').value;
 Year = document.getElementById('year').value;
 Branch = document.getElementById('branch').value;
 RollNo = document.getElementById('participantRNo').value;
 
-console.log("Hello");
+
+let regformSubmit = document.getElementById('regformSubmit');
+regformSubmit.addEventListener('click' signIn);
+
+function signIn(){
+  firebase.auth().createUserWithEmailAndPassword(email, password)
+    .then((userCredential) => {
+      // Signed in 
+      var user = userCredential.user;
+      // ...
+    })
+    .catch((error) => {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // ..
+    });
+}
