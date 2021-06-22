348d14f527b23006e143a86842f8ba7df60c2918
Pratit Shivaji Todkar <pratittodkar@gmail.com>
2021-06-22 22:21:59 +0530

    aded missing year field

--- Authentication/script.js
+++ Authentication/script.js
@@ -3,10 +3,11 @@ function registerAcc() {
   let pass = document.getElementById('password').value;
   let name = document.getElementById('participantName').value;
   let phoneNo = '';
+  let year = '';
   let branch = '';
   let rollNo = '';
   if (emailValidation(email)) {
-    createUser(email, pass, name, phoneNo, branch, rollNo);
+    createUser(email, pass, name, phoneNo, year, branch, rollNo);
   } else {
     alert('Enter The College Email Id!');
   }
