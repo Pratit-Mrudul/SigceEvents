5437f12d613a36814becda414e039b88d8863236
Mrudul Kolambe <83169198+mrudulkolambe@users.noreply.github.com>
2021-06-22 10:54:52 +0530

    update script.js

--- Backend/script.js
+++ Backend/script.js
@@ -1,1 +1,6 @@
-let a = 69;
+Name = document.getElementById('participantName').value;
+Email = document.getElementById('participantEmail').value;
+PhoneNo = document.getElementById('participantPNo').value;
+Year = document.getElementById('year').value;
+Branch = document.getElementById('branch').value;
+RollNo = document.getElementById('participantRNo').value;
