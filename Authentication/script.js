ac090aad0737fe73d9da81558eda958d7f9e1d22
Pratit Shivaji Todkar <pratittodkar@gmail.com>
2021-06-22 21:20:08 +0530

    added name value

--- Authentication/script.js
+++ Authentication/script.js
@@ -3,8 +3,9 @@ const auth = firebase.auth();
 function registerAcc() {
   let email = document.getElementById('participantEmail').value;
   let pass = document.getElementById('password').value;
+  let name = document.getElementById('participantName').value;
   if (validation(email)) {
-    createUser(email, pass);
+    createUser(email, pass, name);
   } else {
     alert('Enter The College Email Id!');
   }
