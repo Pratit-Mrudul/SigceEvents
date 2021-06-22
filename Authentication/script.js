03b38f83eefc415b1a380b427d8f0979125ef8de
Mrudul Kolambe <83169198+mrudulkolambe@users.noreply.github.com>
2021-06-22 23:01:22 +0530

    update script.js

--- Authentication/script.js
+++ Authentication/script.js
@@ -2,13 +2,13 @@ function registerAcc() {
   let email = document.getElementById('participantEmail').value;
   let pass = document.getElementById('password').value;
   let name = document.getElementById('participantName').value;
-  let phoneNo = '';
-  let year = '';
-  let branch = '';
-  let rollNo = '';
+  let phoneNo = document.getElementById('phoneNo').value;
+  let year = document.getElementById('year').value;
+  let branch = document.getElementById('branch').value;
+  let rollNo = document.getElementById('participantRNo').value;
   if (emailValidation(email)) {
     createUser(email, pass, name, phoneNo, year, branch, rollNo);
   } else {
-    alert('Enter The College Email Id!');
+    alert('Enter The Valid Email Id!');
   }
 };
