37448b29600660d84e74c018295b9726db569445
Mrudul Kolambe <83169198+mrudulkolambe@users.noreply.github.com>
2021-06-23 14:22:27 +0530

    update Firebasestore.js

--- Backend/Firebasestore.js
+++ Backend/Firebasestore.js
@@ -1,12 +1,12 @@
 var db = firebase.firestore();
 
 function sendData(mapData) {
-    // Add a new document in collection "cities"
-    db.collection("users").doc(user.getuid).set(mapData)
-    .then(() => {
-        console.log("Document successfully written!");
-    })
-    .catch((error) => {
-        console.error("Error writing document: ", error);
-    });
+  // Add a new document in collection "cities"
+  db.collection("users").doc(name).set(mapData)
+  .then(() => {
+    console.log("Document successfully written!");
+  })
+  .catch((error) => {
+    console.error("Error writing document: ", error);
+  });
 }
