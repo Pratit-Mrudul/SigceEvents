496dd512d6b0edccbb409e02849dbdba5bb8c024
Mrudul Kolambe <83169198+mrudulkolambe@users.noreply.github.com>
2021-06-23 10:33:18 +0530

    update FirebaseAuthentication.js

--- Backend/FirebaseAuthentication.js
+++ Backend/FirebaseAuthentication.js
@@ -3,43 +3,44 @@ const auth = firebase.auth();
 var user
 
 function emailValidation(email) {
-    let domain_name = (email.substring(email.length - 13));
-    if (domain_name == '@sigce.edu.in') {
-      value = true;
-    } else {
-      value = false;
-    }
-    return value;
-  };
+  let domain_name = (email.substring(email.length - 13));
+  if (domain_name == '@sigce.edu.in') {
+    value = true;
+  } else {
+    value = false;
+  }
+  return value;
+};
 
 function createUser(email, password, name, phoneNo, year, branch, rollNo) {
-    firebase.auth().createUserWithEmailAndPassword(email, password)
-    .then((userCredential) => {
-      user = userCredential.user;
-      user.updateProfile({
-        displayName: name,
-      }).then(() => {
-          sendData({
-              "name": name,
-              "email": email,
-              "phoneNo": phoneNo,
-              "year": year,
-              "branch": branch,
-              "rollNo": rollNo,
-          });
-        firebase.auth().currentUser.sendEmailVerification().then(() => {
-            document.getElementById('verficationCollapse').style.display = 'block';
-            document.getElementById('formCollapse').style.display = 'none';
-          });
-      }).catch((error) => {
-        document.getElementById('alertBox').innerHTML = `<div class="alert alert-error" role="alert">
-        ${error}
-      </div>`
+  firebase.auth().createUserWithEmailAndPassword(email, password)
+  .then((userCredential) => {
+    user = userCredential.user;
+    user.updateProfile({
+      displayName: name,
+    }).then(() => {
+      sendData({
+        "name": name,
+        "email": email,
+        "phoneNo": phoneNo,
+        "year": year,
+        "branch": branch,
+        "rollNo": rollNo,
       });
-    })
-    .catch((error) => {
+      firebase.auth().currentUser.sendEmailVerification().then(() => {
+        document.getElementById('verficationCollapse').style.display = 'block';
+        document.getElementById('mailTo').innerHTML = `Email Sent To ${email}!`;
+        document.getElementById('formCollapse').style.display = 'none';
+      });
+    }).catch((error) => {
       document.getElementById('alertBox').innerHTML = `<div class="alert alert-error" role="alert">
-      ${error.message}
-    </div>`
+      ${error}
+      </div>`
     });
+  })
+  .catch((error) => {
+    document.getElementById('alertBox').innerHTML = `<div class="alert alert-error" role="alert">
+    ${error.message}
+    </div>`
+  });
 }
