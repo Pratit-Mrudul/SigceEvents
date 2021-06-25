34f2279d25e36a2aafea561e3da4a95172e0fd7d
Pratit Shivaji Todkar <pratittodkar@gmail.com>
2021-06-26 01:20:46 +0530

    edit

--- Credits/script.js
+++ Credits/script.js
@@ -4,12 +4,3 @@ const container = document.querySelector(".container");
 hamburger_menu.addEventListener("click", () => {
   container.classList.toggle("active");
 });
-hamburger_menu.addEventListener("click", () => {
-  card.classList.toggle("opacitytoggle");
-});
-hamburger_menu.addEventListener("click", () => {
-  sigceevents.classList.toggle("opacitytoggle2");
-});
-hamburger_menu.addEventListener("click", () => {
-  main.classList.toggle("blurcontainer");
-});
