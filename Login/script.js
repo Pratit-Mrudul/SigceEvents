2d570433674f396180bcf48cffbc785fdcf96fcd
Mrudul Kolambe <83169198+mrudulkolambe@users.noreply.github.com>
2021-06-25 08:46:29 +0530

    update script.js

--- Login/script.js
+++ Login/script.js
@@ -8,12 +8,11 @@ hamburger_menu.addEventListener("click", () => {
 function getLoginData() {
   let loginEmail = document.getElementById('loginEmail').value;
   let LoginPw = document.getElementById('LoginPw').value;
+  setTimeout(function() {
+    document.getElementById('circeloader').style.display = '';
+  }, 100);
 
   if (emailValidation(loginEmail)) {
     login(loginEmail, LoginPw);
-    setTimeout(function() {
-      document.getElementById('circeloader').style.display = '';
-    }, 100);
-
   }
 }
