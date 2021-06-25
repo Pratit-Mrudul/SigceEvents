c078981c6a85b2b6a04e581436daf10567870a8e
Mrudul Kolambe <83169198+mrudulkolambe@users.noreply.github.com>
2021-06-25 15:34:09 +0530

    update script.js

--- Authentication/script.js
+++ Authentication/script.js
@@ -17,19 +17,19 @@ function registerAcc() {
 function branchToggle() {
   let year = document.getElementById('year').value;
   if (year == 'FY') {
-    document.getElementById('branch').style.display = '';
+    document.getElementById('branch').style.display = 'inline-block';
     document.getElementById('branch2').style.display = 'none';
     document.getElementById('branch3').style.display = 'none';
   } else if (year == 'SY') {
     document.getElementById('branch').style.display = 'none';
-    document.getElementById('branch2').style.display = '';
+    document.getElementById('branch2').style.display = 'inline-block';
     document.getElementById('branch3').style.display = 'none';
   } else if (year == 'TY') {
     document.getElementById('branch').style.display = 'none';
     document.getElementById('branch2').style.display = 'none';
-    document.getElementById('branch3').style.display = '';
+    document.getElementById('branch3').style.display = 'inline-block';
   } else if (year == 'select') {
-    document.getElementById('branch').style.display = '';
+    document.getElementById('branch').style.display = 'inline-block';
     document.getElementById('branch2').style.display = 'none';
     document.getElementById('branch3').style.display = 'none';
   }
