690ed5c4c275ba01ff53a9c5ebb6e93c41ef387f
Pratit Shivaji Todkar <pratittodkar@gmail.com>
2021-06-26 14:48:10 +0530

    edit

--- Notification/script.js
+++ Notification/script.js
@@ -4,3 +4,14 @@ const container = document.querySelector(".container");
 hamburger_menu.addEventListener("click", () => {
   container.classList.toggle("active");
 });
+
+CloseButtons = document.getElementsByClassName('closeButton');
+
+for (button in CloseButtons, noti in notificationList) {
+  button.addEventListener('click', () => {
+    let email = user.email;
+    data = {};
+    data[email] = firebase.firestore.FieldValue.delete();
+    db.collection('events').doc(noti).update(data);
+  })
+}
