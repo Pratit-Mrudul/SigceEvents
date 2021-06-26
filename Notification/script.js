9e1cda281f0288faad50ae68beec6286d5fa9ec7
Pratit Shivaji Todkar <pratittodkar@gmail.com>
2021-06-26 15:36:18 +0530

    edit

--- Notification/script.js
+++ Notification/script.js
@@ -10,12 +10,12 @@ firebase.auth().onAuthStateChanged(async (user) => {
   if (user) {
     await getDocumentData(db.collection("users").doc(user.uid));
     let notificationList = userData['participatedEvents'];
-    for (button in CloseButtons, noti in notificationList) {
+    for (let i=0; i<CloseButtons.length; i++) {
       button.addEventListener('click', () => {
         let email = user.email;
         data = {};
         data[email] = firebase.firestore.FieldValue.delete();
-        db.collection('events').doc(noti).update(data);
+        db.collection('events').doc(notificationList[i]).update(data);
       })
     };
   }}
