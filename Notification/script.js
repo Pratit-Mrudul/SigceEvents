bbb95d69704a57f1ec44e81398083f6cd0f055cd
Pratit Shivaji Todkar <pratittodkar@gmail.com>
2021-06-26 15:47:02 +0530

    edit

--- Notification/script.js
+++ Notification/script.js
@@ -4,19 +4,3 @@ const container = document.querySelector(".container");
 hamburger_menu.addEventListener("click", () => {
   container.classList.toggle("active");
 });
-
-CloseButtons = document.getElementsByClassName('closeButton');
-firebase.auth().onAuthStateChanged(async (user) => {
-  if (user) {
-    await getDocumentData(db.collection("users").doc(user.uid));
-    let notificationList = userData['participatedEvents'];
-    for (let i=0; i<=CloseButtons.length; i++) {
-      button.addEventListener('click', () => {
-        let email = user.email;
-        data = {};
-        data[email] = firebase.firestore.FieldValue.delete();
-        db.collection('events').doc(notificationList[i]).update(data);
-      })
-    };
-  }}
-);
