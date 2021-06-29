df0a79bd713eccb9f476f08f936caa155c08cbed
Pratit Shivaji Todkar <pratittodkar@gmail.com>
2021-06-30 03:12:53 +0530

    edit

--- Teams/script.js
+++ Teams/script.js
@@ -112,8 +112,8 @@ function sortEntries() {
 
   // Loop through all list items, and hide those who don't match the search query
   for (i = 0; i < li.length; i++) {
-    a = li[i].getElementsByTagName("a")[0];
-    txtValue = a.textContent || a.innerText;
+    p = li[i].getElementsByTagName("p")[0];
+    txtValue = p.textContent || p.innerText;
     if (txtValue.toUpperCase().indexOf(filter) > -1) {
       li[i].style.display = "";
     } else {
