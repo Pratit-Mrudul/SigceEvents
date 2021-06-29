dda8e60189ac5fd9f9938c59140c6834172c328e
Pratit Shivaji Todkar <pratittodkar@gmail.com>
2021-06-30 03:11:52 +0530

    edited

--- Teams/script.js
+++ Teams/script.js
@@ -104,12 +104,20 @@ function generateList() {
 }
 
 function sortEntries() {
-  document.getElementsByClassName("userFill")[0].style.display = 'none';
-  $('.userData').sort(function(a, b) {
-    if (a.innerText < b.innerText) {
-      return -1;
+  // Declare variables
+  var input, filter, li, a, i, txtValue;
+  input = document.getElementById('searchInput');
+  filter = input.value.toUpperCase();
+  li = document.getElementsByClassName('userData');
+
+  // Loop through all list items, and hide those who don't match the search query
+  for (i = 0; i < li.length; i++) {
+    a = li[i].getElementsByTagName("a")[0];
+    txtValue = a.textContent || a.innerText;
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      li[i].style.display = "";
     } else {
-      return 1;
+      li[i].style.display = "none";
     }
-  }).appendTo('.userFillSearch');
+  }
 }
