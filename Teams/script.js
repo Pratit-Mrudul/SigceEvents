ba864ff3dded0c1cbd08462edbd41c8277f78101
Mrudul Kolambe <83169198+mrudulkolambe@users.noreply.github.com>
2021-06-29 23:32:36 +0530

    update script.js

--- Teams/script.js
+++ Teams/script.js
@@ -2,27 +2,32 @@ const hamburger_menu = document.querySelector(".hamburger-menu");
 const container = document.querySelector(".container");
 const BGMIMember = document.getElementById('BGMIMember');
 const QUIZMember = document.getElementById('QUIZMember');
+const IDEASMember = document.getElementById('IDEASMember');
 const DEBATEMember = document.getElementById('DEBATEMember');
 const formbackContainer = document.getElementById('formbackContainer');
 const participantsDisplay = document.getElementById('participantsDisplay');
 const BGMIP = document.getElementById('BGMIP');
 const QUIZP = document.getElementById('QUIZP');
+const IDEASP = document.getElementById('IDEASP');
 const DEBATEP = document.getElementById('DEBATEP');
 const backbtn = document.getElementsByClassName('backbtn');
 const BGMI = document.getElementsByClassName("BGMI");
 const QUIZ = document.getElementsByClassName("QUIZ");
+const IDEAS = document.getElementsByClassName("IDEAS");
 const DEBATE = document.getElementsByClassName("DEBATE");
 var selectedEvent = "";
 
 BGMIMember.addEventListener('click', bgmiTeams);
 QUIZMember.addEventListener('click', quizTeams);
 DEBATEMember.addEventListener('click', debateTeams);
+IDEASMember.addEventListener('click', ideasTeams);
 
 function bgmiTeams() {
   formbackContainer.style.display = 'none';
   participantsDisplay.style.display = 'block';
   QUIZP.style.display = 'none';
   DEBATEP.style.display = 'none';
+  IDEASP.style.display = 'none';
   BGMIP.style.display = 'block';
   selectedEvent = "BGMI";
 }
@@ -32,6 +37,7 @@ function quizTeams() {
   participantsDisplay.style.display = 'block';
   QUIZP.style.display = 'block';
   DEBATEP.style.display = 'none';
+  IDEASP.style.display = 'none';
   BGMIP.style.display = 'none';
   selectedEvent = "QUIZ";
 }
@@ -41,8 +47,19 @@ function debateTeams() {
   participantsDisplay.style.display = 'block';
   QUIZP.style.display = 'none';
   DEBATEP.style.display = 'block';
+  IDEASP.style.display = 'none';
   BGMIP.style.display = 'none';
   selectedEvent = "DEBATE";
+}
+
+function ideasTeamsTeams() {
+  formbackContainer.style.display = 'none';
+  participantsDisplay.style.display = 'block';
+  QUIZP.style.display = 'none';
+  DEBATEP.style.display = 'none';
+  IDEASP.style.display = 'block';
+  BGMIP.style.display = 'none';
+  selectedEvent = "IDEAS";
 }
 function backBtn() {
   formbackContainer.style.display = '';
@@ -51,10 +68,10 @@ function backBtn() {
 
 db.collection("users").where("participatedEvents", "array-contains", selectedEvent).get().then((querySnapshot) => {
   querySnapshot.forEach((doc) => {
-      // doc.data() is never undefined for query doc snapshots
-      Data = doc.data();
-      memberTemplate = `<p>Name: ${Data["name"]}  Email: ${Data["email"]}<button><i class="fas fa-user-plus users"></i></button></p>`;
-      document.getElementsByClassName(`${selectedEvent}P`).innerHTML += memberTemplate;
+    // doc.data() is never undefined for query doc snapshots
+    Data = doc.data();
+    memberTemplate = `<p>Name: ${Data["name"]}  Email: ${Data["email"]}<button><i class="fas fa-user-plus users"></i></button></p>`;
+    document.getElementsByClassName(`${selectedEvent}P`).innerHTML += memberTemplate;
   });
 })
 .catch((error) => {
