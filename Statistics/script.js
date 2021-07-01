fa79d6a31b0f469dc98b9ce64070e32dd046d245
Pratit Shivaji Todkar <pratittodkar@gmail.com>
2021-07-01 22:07:31 +0530

    added statistics

--- Statistics/script.js
+++ Statistics/script.js
@@ -4,3 +4,23 @@ const container = document.querySelector(".container");
 hamburger_menu.addEventListener("click", () => {
   container.classList.toggle("active");
 });
+
+db.collection("events").get()
+    .then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+            let data = doc.data();
+            let count = 0;
+            for(index in data) {
+              count += 1;
+            }
+            document.getElementById('table').innerHTML += `
+              <tr id="row">
+                <td>${doc.id}</td>
+                <td>${count}</td>
+              </tr>
+              `
+        });
+    })
+    .catch((error) => {
+        console.log("Error getting documents: ", error);
+    });
