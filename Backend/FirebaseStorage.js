65487277b2f70d74d6c39c102df45fd434baad6d
Pratit Shivaji Todkar <pratittodkar@gmail.com>
2021-07-02 00:09:39 +0530

    edited submissions

--- Backend/FirebaseStorage.js
+++ Backend/FirebaseStorage.js
@@ -1,8 +1,18 @@
 const ref = firebase.storage().ref();
+var numberOfFiles = 1
+var fileTypes = [];
+let eventName = '';
 
 async function sendFiles() {
-    const file = await document.querySelector('#file').files[0];
-    const name = (+new Date()) + '-' + file.name;
-    const metadata = { contentType: file.type };
-    await ref.child('Photography').put(file, metadata).then(() => {console.log('done')});
+    let submittedFiles = document.querySelector('#file').files;
+    if (submittedFiles.length == numberOfFiles) {
+        for (let i = 0; i < numberOfFiles; i++ ) {
+            const file = await document.querySelector('#file').files[i];
+            const name = (+new Date()) + '-' + file.name;
+            if (fileTypes.includes(file.type)) {
+                const metadata = { contentType: file.type };
+                await ref.child(eventName).put(file, metadata).then(() => {console.log('done')});
+            }
+        }
+    }
 }
--- Submissions/script.js
+++ Submissions/script.js
@@ -15,3 +15,23 @@ function changeButton() {
   Labelbtn.innerHTML = 'Upload';
   Labelbtn.setAttribute('for', '');
 }
+
+function info() {
+  eventName = document.getElementById('event').value;
+  if (eventName == "PHOTOGRAPHY"){
+    numberOfFiles = 5
+    fileTypes = ["image/jpg", "image/jpeg", "image/png", "image/DNG", "image/NEF"]
+  } else if (eventName == "DANCE") {
+    fileTypes = ["video/x-matroska", "video/mp4"]
+  } else if (eventName == "NAIL-ART") {
+    fileTypes = ["video/x-matroska", "video/mp4"]
+  } else if (eventName == "INSTRUMENT") {
+    fileTypes = ["video/x-matroska", "video/mp4"]
+  } else if (eventName == "SKETCH") {
+    fileTypes = ['.jpg', '.jpeg', '.png']
+  } else if (eventName == "CHALK") {
+    fileTypes = ["video/x-matroska", "video/mp4"]
+  } else if (eventName == "POSTER") {
+    fileTypes = ["image/jpg", "image/jpeg", "image/png"]
+  }
+}
