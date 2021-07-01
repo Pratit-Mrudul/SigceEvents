05be72c6db5fc77ca61b932e5f3b43e054b24d18
Pratit Shivaji Todkar <pratittodkar@gmail.com>
2021-07-02 00:18:22 +0530

    edited submissions

--- Submissions/script.js
+++ Submissions/script.js
@@ -1,6 +1,6 @@
 const hamburger_menu = document.querySelector(".hamburger-menu");
 const container = document.querySelector(".container");
-const eventSelector = document.querySelector("#selectEvent");
+const eventSelector = document.getElementById("selectEvent");
 const fileUpload = document.getElementsByClassName('formback')[1];
 
 hamburger_menu.addEventListener("click", () => {
@@ -21,19 +21,33 @@ function changeButton() {
 function info() {
   eventName = document.getElementById('event').value;
   if (eventName == "PHOTOGRAPHY"){
+    eventSelector.style.display = 'none';
+    fileUpload.style.display = '';
     numberOfFiles = 5
     fileTypes = ["image/jpg", "image/jpeg", "image/png", "image/DNG", "image/NEF"]
   } else if (eventName == "DANCE") {
+    eventSelector.style.display = 'none';
+    fileUpload.style.display = '';
     fileTypes = ["video/x-matroska", "video/mp4"]
   } else if (eventName == "NAIL-ART") {
+    eventSelector.style.display = 'none';
+    fileUpload.style.display = '';
     fileTypes = ["video/x-matroska", "video/mp4"]
   } else if (eventName == "INSTRUMENT") {
+    eventSelector.style.display = 'none';
+    fileUpload.style.display = '';
     fileTypes = ["video/x-matroska", "video/mp4"]
   } else if (eventName == "SKETCH") {
+    eventSelector.style.display = 'none';
+    fileUpload.style.display = '';
     fileTypes = ['.jpg', '.jpeg', '.png']
   } else if (eventName == "CHALK") {
+    eventSelector.style.display = 'none';
+    fileUpload.style.display = '';
     fileTypes = ["video/x-matroska", "video/mp4"]
   } else if (eventName == "POSTER") {
+    eventSelector.style.display = 'none';
+    fileUpload.style.display = '';
     fileTypes = ["image/jpg", "image/jpeg", "image/png"]
   }
 }
